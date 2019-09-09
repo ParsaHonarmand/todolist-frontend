7880e649485b6e4f9a616ebd16600adc5452233e
parsa Honarmand <parsa.Honarmand@gmail.com>
2019-09-09 11:14:16 -0600

    minor additions to css

--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -3,11 +3,12 @@ import React from 'react';
 function ToDoItems(props){
     return (
         props.wholeList.map((char, index) => 
-            <div role="list" className="ui divided middle aligned list">
+            <div id="todoList" role="list" className="ui divided middle aligned list">
                 <div role="listitem" className="item">
-                    {char}
+                    <span id="lines">{char}</span>
                     <div className="right floated content">
-                        <button className="ui mini button" key={index} index={index} onClick={props.onDelete}>X</button>
+                        <button className="ui mini button" value={index} key={index} index={index} onClick={props.onComplete}><i aria-hidden="true" class="checkmark icon"></i></button>
+                        <button className="ui mini button" value={index} key={index} index={index} onClick={props.onDelete}>X</button>
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -27,32 +27,56 @@ class App extends React.Component {
     }
 
 
-    deleteToDo(itemIndex){
-        if (itemIndex === 'undefined') {      
-            console.log(itemIndex)    
+    deleteToDo(e){
+        if (e === 'undefined') {      
+            console.log(e)    
         }
         const newToDos = this.state.items
-        newToDos.splice(itemIndex, 1)
+        newToDos.splice(e.currentTarget.value, 1)
         this.setState({
             items: newToDos
         })
     }
 
+
     render(){
-        return(
-            <div>
-                <div id="header">
-                    <h2>Welcome to your ToDo list</h2>
-                    <form className="ui medium form" onSubmit={this.addToDo.bind(this)}>
-                        <input placeholder="What do you need to do?" type='text' onChange={this.handleInput.bind(this)}/>
-                        <button className="ui button">ADD</button>
-                    </form>
+        var date = new Date().getDate()
+        var month = new Date().getMonth()
+        var year = new Date().getFullYear()
+        if (this.state.items.length === 0) {
+            return(
+                <div>
+                    <div id="header">
+                        <h2>Welcome to your ToDo list</h2>
+                        <p>TODAY'S DATE: {date}/{month}/{year}</p> 
+                        <form className="ui medium form" onSubmit={this.addToDo.bind(this)}>
+                            <input placeholder="What do you need to do?" type='text' onChange={this.handleInput.bind(this)}/>
+                            <button id="addBut" className="ui button">ADD</button>
+                        </form>
+                    </div>
+                    <div id="list">
+                        <h2 id="header">Your list is currently empty!</h2>
+                    </div>            
                 </div>
-                <div id="list">
-                    <ToDoItems id="list" listItem={this.state.currentItem} wholeList={this.state.items} onDelete={this.deleteToDo.bind(this)} />
+            )
+        }
+        else {
+            return(
+                <div>
+                    <div id="header">
+                        <h2>Welcome to your ToDo list</h2>
+                        <p>TODAY'S DATE: {date}/{month}/{year}</p> 
+                        <form className="ui medium form" onSubmit={this.addToDo.bind(this)}>
+                            <input placeholder="What do you need to do?" type='text' onChange={this.handleInput.bind(this)}/>
+                            <button className="ui button">ADD</button>
+                        </form>
+                    </div>
+                    <div id="list">
+                        <ToDoItems id="list" listItem={this.state.currentItem} wholeList={this.state.items} onDelete={this.deleteToDo.bind(this)} />
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 }
 
