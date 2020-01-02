1ce8abcc38df383b86d832de0c00dfcda5304733
parsa Honarmand <parsa.Honarmand@gmail.com>
2020-01-02 16:42:49 -0700

    fixed some requests

--- src/Login.js
+++ src/Login.js
@@ -48,13 +48,14 @@ class Login extends React.Component {
         }
         
         const getItems = async () => {
-            let itemsToSend = await axios.post(process.env.REACT_APP_BACK_END_URL + 'getUser', userObj)
+            const itemsToSend = await axios.post(process.env.REACT_APP_BACK_END_URL + 'getUser', userObj)
                 .then(res => console.log(res.data[0]))
                 .catch(err => console.log(err))
             // this.setState({
             //     todos: itemsToSend.data[0]
             // })
-            console.log(itemsToSend)
+            let data = itemsToSend.data
+            console.log(data)
         }
         getItems()
         //reset at the end
