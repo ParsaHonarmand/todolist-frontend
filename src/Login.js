8402add6320512d13bfb2d7fbc34ed39f959a2c2
parsa Honarmand <parsa.Honarmand@gmail.com>
2020-01-02 17:29:15 -0700

    fixed some requests

--- src/Login.js
+++ src/Login.js
@@ -14,6 +14,7 @@ const axios = require('axios');
 class Login extends React.Component {
     constructor(props) {
         super(props)
+        this.validateLogin = this.validateLogin.bind(this)
         this.state = {
             username: "",
             password: "",
