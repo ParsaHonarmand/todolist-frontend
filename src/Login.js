651f3d1f3e63b38fe0450f0016c99fbce6e039d7
parsa Honarmand <parsa.Honarmand@gmail.com>
2020-01-02 17:03:13 -0700

    fixed some requests

--- src/Login.js
+++ src/Login.js
@@ -14,6 +14,7 @@ const axios = require('axios');
 class Login extends React.Component {
     constructor(props) {
         super(props)
+        this.getItems = this.getItems.bind(this)
         this.state = {
             username: "",
             password: "",
